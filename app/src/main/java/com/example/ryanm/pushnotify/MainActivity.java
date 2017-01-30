d166af8e337ab60603de8d1bf170f304f4af11c3
ryanmalencia <ryan.malencia@pitt.edu>
2017-01-30 02:56:30 -0500

    Implemented Bottom nav bar

--- app/src/main/java/com/example/ryanm/pushnotify/MainActivity.java
+++ app/src/main/java/com/example/ryanm/pushnotify/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.ryanm.pushnotify;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import static com.example.ryanm.pushnotify.R.id.bottom_navigation;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +17,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(this, RegistrationService.class);
         startService(intent);
+
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+        new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_campus:
+
+                    case R.id.action_concerts:
+
+                    case R.id.action_sports:
+
+                    case R.id.action_clubs:
+
+                    case R.id.action_other:
+
+                }
+                return true;
+            }
+        });
     }
 }
