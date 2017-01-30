b993ba6d371b866c2a2617fc97e4c95a0b29656d
ryanmalencia <ryan.malencia@pitt.edu>
2017-01-29 23:57:24 -0500

    Mockup for receiving data from gcm

--- app/src/main/java/com/example/ryanm/pushnotify/NotificationsListenerService.java
+++ app/src/main/java/com/example/ryanm/pushnotify/NotificationsListenerService.java
@@ -1,7 +1,14 @@
 package com.example.ryanm.pushnotify;
 
+import android.os.Bundle;
+
 import com.google.android.gms.gcm.GcmListenerService;
 
 
 public class NotificationsListenerService extends GcmListenerService {
+    @Override
+    public void onMessageReceived(String from, Bundle data){
+        System.out.println(from);
+        System.out.println(data.get("message"));
+    }
 }
