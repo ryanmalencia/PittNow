fa085c8ff9c805b53beff81f76c135613d528ee9
ryanmalencia <ryan.malencia@pitt.edu>
2017-02-06 23:49:33 -0500

    Added google sign in capabilites.

--- app/src/main/java/com/example/ryanm/pushnotify/DataTypes/AppDevice.java
+++ app/src/main/java/com/example/ryanm/pushnotify/DataTypes/AppDevice.java
@@ -4,13 +4,15 @@ package com.example.ryanm.pushnotify.DataTypes;
 public class AppDevice {
     private int AppDeviceID;
     private String RegID;
+    //private String GoogID;
 
     /**
      * Class that represents a Physical Device
      * @param reg_id the Registration ID of the Device
      */
-    public AppDevice (String reg_id)
+    public AppDevice (String reg_id)//, String goog_id)
     {
         RegID = reg_id;
+        //GoogID = goog_id;
     }
 }
--- app/src/main/java/com/example/ryanm/pushnotify/SplashScreen.java
+++ app/src/main/java/com/example/ryanm/pushnotify/SplashScreen.java
@@ -4,14 +4,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.io.File;
+
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        String filename = "auth";
+
+        File file = new File(getFilesDir() + filename);
+
+        if(!file.exists()) {
+            Intent intent = new Intent(this, SignIn.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
